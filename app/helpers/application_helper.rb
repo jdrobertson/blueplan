4fb4b2c37f24c9cdcc81ef931b344c118794bda6
Joel Robertson <joel.robertson@powershop.co.nz>
2017-01-12 21:29:48 +1300

    Added title helper to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def site_name
     "Blueplan"
   end
+
+  def full_title(page_title = '')
+    base_title = site_name
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
