03ebc17032aa856fffcc5cccc6d87b659ad5ba61
Joel Robertson <joel.robertson@powershop.co.nz>
2017-01-12 21:15:16 +1300

    misc changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def new
-    new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'homes#show'
   resource :home
+  resources :users
   resources :songs
   resources :events do
     resources :scheduled_songs
