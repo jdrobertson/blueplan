798d92b8318346a07a0303bdb45f95a43f295082
Joel Robertson <joel.robertson@powershop.co.nz>
2017-06-27 11:31:07 +1200

    Lots of things

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'homes#show'
   resource :home
+  resource :session
   resources :users
   resources :songs
   resources :events do
     resources :scheduled_songs
   end
+
 end
